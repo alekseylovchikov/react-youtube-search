e3aa5f08e2cb42b79365ccb49fe4bb9fe2be8bac
alekseylovchikov <svdmusic@gmail.com>
2016-01-24 23:31:54 +0300

    add component controller

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,14 +4,13 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: 'Here you see input value...' };
+    this.state = { term: '' };
   }
 
   render() {
     return (
       <div>
-        <input onChange={event => this.setState({ term: event.target.value })} />
-        <br />Value of input: {this.state.term}
+        <input value={this.state.term} onChange={event => this.setState({ term: event.target.value })} />
       </div>
     );
     // return <input onClick={event => console.log(event.target.value)} />
